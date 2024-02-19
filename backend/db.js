21a6ac9bb83921600501621edf1b6d328c9898a8
larrikin-coder <shauryat.22703@gmail.com>
2024-02-20 01:14:21 +0530

    express and mongooose active!

---
+++ backend/db.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+const mongoURI = "mongodb://0.0.0.0:27017/";//replace localhost with 0.0.0.0
+
+const connectToMongo = () =>{
+    mongoose.connect(mongoURI,)
+    .then(()=>{
+        console.log("connected successfully");
+    })
+    .catch((err)=>{
+        console.log(err);
+    })
+}
+
+module.exports = connectToMongo;
