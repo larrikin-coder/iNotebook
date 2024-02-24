94ce760c3037b9b01ab01717f7d464465c277b5b
larrikin-coder <shauryat.22703@gmail.com>
2024-02-24 15:50:10 +0530

    authetication done...thunderclient collection created for createuser

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -21,17 +21,24 @@ router.post(
     //Creating a user
 
     // Check whether user with this email exists already
-    let user = user.findOne({email: req.body.email});
-    if(user){
-        return res.status(400).json({error: "Sorry a user already exists with this email"})
+    try{
+
+      let user = await User.findOne({email: req.body.email});
+      if(user){
+          return res.status(400).json({error: "Sorry a user already exists with this email"})
+      }
+      user = await User.create({
+          name: req.body.name,
+          email: req.body.email,
+          password: req.body.password
+      })
+      
+      res.send(req.body);
     }
-    user = await User.create({
-        name: req.body.name,
-        email: req.body.email,
-        password: req.body.password
-    })
-    
-    // res.send(req.body);
+    catch(error){
+      console.error(error.message);
+      res.status(500).send("Some Error Occured")
+    }
 
   }
 );
